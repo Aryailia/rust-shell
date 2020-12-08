f21b39e8e7a2d8f3d5b9f495d1c574e05fcd11fb
Aryailia <Aryailia@users.noreply.github.com>
2020-12-08 11:40:47 +0800

    Commit last changes

--- src/run.rs
+++ src/run.rs
@@ -7,9 +7,20 @@ use futures::stream::{Stream, StreamExt};
 
 
 pub async fn job_stream_run(mut input: UnboundedReceiver<Parseme>) {
-    let mut row = 0;
+    let mut program = Vec::new();
     while let Some(stmt) = input.next().await {
-        println!("{} | {:?}", row, stmt);
-        row += 1;
+
+        match stmt {
+            Parseme::Statement(cmd) => {
+                //Command::new()
+                program.push(Parseme::Statement(cmd));
+            }
+            p => program.push(p),
+            //_ => println!("{} | {:?}", row, stmt),
+        }
     }
+    program.iter()
+        .enumerate()
+        .for_each(|(row, stmt)| println!("{} | {:?}", row, stmt));
+    //println!("{:?}", program);
 }
